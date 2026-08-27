4eda417f4ebec1163f0d95b60884c3724609b2b5
Lucas <secnuc27@gmail.com>
2026-08-26 22:22:49 -0500

    Corrige caminho pro Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/sos-assi/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
